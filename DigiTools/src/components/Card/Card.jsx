0d40307948069b9885275ec0242af88531de0bd9
SharifMuhammadSifat <166907907+SharifMuhammadSifat@users.noreply.github.com>
2026-04-17 00:38:51 +0600

    completed card front end

--- DigiTools/src/components/Card/Card.jsx
+++ DigiTools/src/components/Card/Card.jsx
@@ -3,9 +3,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 const Card = ({ product }) => {
-    console.log(product);
+    console.log(product.tag);
     return (
-        <div className='flex flex-col justify-between gap-2 p-5 border rounded-lg shadow-md relative'>
+        <div className='flex flex-col justify-between gap-2 p-5 border border-[#d8cbcb] rounded-lg shadow-md relative'>
             <img className='w-10 h-10' src={product.icon} alt={product.name} />
             <h2>{product.name}</h2>
             <p>{product.description}</p>
@@ -16,7 +16,9 @@ const Card = ({ product }) => {
                     style={{ color: "rgb(30, 232, 50)" }}
                 /> {feature}</p>)
             }
-            <button className='bg-linear-to-r from-blue-500 to-purple-600 text-white border-0 rounded-full py-2 px-4 hover:from-blue-600 hover:to-purple-700'>Buy Now</button>
+
+            <p className = {`absolute top-2 right-2 text-[12px] font-medium  px-2 py-1 rounded-full ${product.tag === "new" ? "text-[#0a883e] bg-[#dbfce7]" : product.tag ==="best seller" ? "text-[#bb4d00] bg-[#fef3c6]" : "text-purple-600 bg-[#e1e7ff]"} `}>{product.tag}</p>
+            <button className='btn bg-linear-to-r from-blue-500 to-purple-600 text-white border-0 rounded-full py-2 px-4 hover:from-blue-600 hover:to-purple-700'>Buy Now</button>
         </div>
     );
 };
